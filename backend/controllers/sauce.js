7e7de73f96f5fcad266f5523f7247f4b6df5b0b9
Solène Viogeat <so.gd@hotmail.fr>
2022-07-14 15:52:24 +0200

    Création d'une sauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -3,67 +3,24 @@ const fs = require('fs');
 
 //Logique métier pour créer un nouvel utilisateur
 
+exports.createSauce = (req, res, next) => {
+    const sauceObject = JSON.parse(req.body.sauce);
+    console.log(sauceObject)
+    delete sauceObject._id;
+    delete sauceObject.userId;
 
-
-//Logique métier pour modifier un utilisateur
-
-exports.modifySauce = (req, res, next) => {
-    const sauceObject = req.file ? {
-        ...JSON.parse(req.body.sauce),
+    const sauce = new Sauce({
+        ...sauceObject,
+        userId: req.auth.userId,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : { ...req.body };
+    });
+  console.log(sauce)
   
-    delete sauceObject._userId;
-    Sauce.findOne({_id: req.params.id})
-        .then((sauce) => {
-            if (sauce.userId != req.auth.userId) {
-                res.status(401).json({ message : 'Not authorized'});
-            } else {
-                Sauce.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
-                .then(() => res.status(200).json({message : 'Objet modifié!'}))
-                .catch(error => res.status(401).json({ error }));
-            }
-        })
-        .catch((error) => {
-            res.status(400).json({ error });
-        });
- };
-
-//Logique métier pour supprimer un utilisateur
-
-exports.deleteSauce = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id})
-        .then(sauce => {
-            if (sauce.userId != req.auth.userId) {
-                res.status(401).json({message: 'Not authorized'});
-            } else {
-                const filename = sauce.imageUrl.split('/images/')[1];
-                fs.unlink(`images/${filename}`, () => {
-                    Sauce.deleteOne({_id: req.params.id})
-                        .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
-                        .catch(error => res.status(401).json({ error }));
-                });
-            }
-        })
-        .catch( error => {
-            res.status(500).json({ error });
-        });
- };
-
- //Logique métier pour récupérer tous les utilisateurs
-
-exports.getAllSauces = (req, res, next) => {
-    Sauce.find()
-    .then(sauces => res.status(200).json(sauces))
-    .catch(error => res.status(400).json({ error }));
-
-}
-
-//Logique métier pour récupérer un utilisateur
-
-exports.getOneSauce = (req, res, next) => {
-Sauce.findOne({ _id: req.params.id })
-    .then(sauce => res.status(200).json(sauce))
-    .catch(error => res.status(404).json({ error }));
+    sauce.save()
+    .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
+    .catch(error => { 
+        console.log(error)
+        res.status(400).json( { error })
+    })
 }
 
