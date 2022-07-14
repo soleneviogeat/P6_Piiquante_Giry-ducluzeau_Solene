a6f2ce26191657e34a05886535e93cf1ad974bcc
Solène Viogeat <so.gd@hotmail.fr>
2022-07-14 15:53:30 +0200

    Suppression d'une sauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -48,5 +48,26 @@ exports.modifySauce = (req, res, next) => {
         });
  };
 
+//Logique métier pour supprimer un utilisateur
+
+exports.deleteSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id})
+        .then(sauce => {
+            if (sauce.userId != req.auth.userId) {
+                res.status(401).json({message: 'Not authorized'});
+            } else {
+                const filename = sauce.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    Sauce.deleteOne({_id: req.params.id})
+                        .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
+                        .catch(error => res.status(401).json({ error }));
+                });
+            }
+        })
+        .catch( error => {
+            res.status(500).json({ error });
+        });
+ };
+
 
 
