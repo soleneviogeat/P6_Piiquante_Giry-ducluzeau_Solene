ef9edf36bdc37e7f238333edddabeea31396f307
Solène Viogeat <so.gd@hotmail.fr>
2022-07-14 15:54:32 +0200

    Visualisation des sauces

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -46,7 +46,7 @@ exports.modifySauce = (req, res, next) => {
         .catch((error) => {
             res.status(400).json({ error });
         });
- };
+};
 
 //Logique métier pour supprimer un utilisateur
 
@@ -67,7 +67,22 @@ exports.deleteSauce = (req, res, next) => {
         .catch( error => {
             res.status(500).json({ error });
         });
- };
+};
 
+//Logique métier pour récupérer tous les utilisateurs
 
+exports.getAllSauces = (req, res, next) => {
+    Sauce.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({ error }));
+
+}
+
+//Logique métier pour récupérer un utilisateur
+
+exports.getOneSauce = (req, res, next) => {
+Sauce.findOne({ _id: req.params.id })
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(404).json({ error }));
+}
 
