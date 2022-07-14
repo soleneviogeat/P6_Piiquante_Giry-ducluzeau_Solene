6e5320276268021ddcb90e6dd40d89f4dd5aeb07
Solène Viogeat <so.gd@hotmail.fr>
2022-07-14 15:52:58 +0200

    Modification d'une sauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -24,3 +24,29 @@ exports.createSauce = (req, res, next) => {
     })
 }
 
+//Logique métier pour modifier un utilisateur
+
+exports.modifySauce = (req, res, next) => {
+    const sauceObject = req.file ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+  
+    delete sauceObject._userId;
+    Sauce.findOne({_id: req.params.id})
+        .then((sauce) => {
+            if (sauce.userId != req.auth.userId) {
+                res.status(401).json({ message : 'Not authorized'});
+            } else {
+                Sauce.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
+                .then(() => res.status(200).json({message : 'Objet modifié!'}))
+                .catch(error => res.status(401).json({ error }));
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error });
+        });
+ };
+
+
+
